ab029c0fa827f9a27941247c3767a45bd7e7a3aa
EttTer <tereza.ettlerova@gmail.com>
2023-10-12 08:01:11 +0200

    first and second

--- src/components/Wordbox/index.jsx
+++ src/components/Wordbox/index.jsx
@@ -1,3 +1,41 @@
+
+import React, { useState, useEffect } from 'react';
+import './style.css';
+
+const Wordbox = ({ word, onFinish }) => {
+  const [lettersLeft, setLettersLeft] = useState(word);  
+
+  useEffect(() => {
+    const handleKeyUp = (event) => {
+      const userInput = event.key;
+      if (lettersLeft.includes(userInput)) {
+        const updatedLetters = lettersLeft.replace(userInput, '');
+        setLettersLeft(updatedLetters);
+
+        if (updatedLetters === '') {
+          onFinish(); // Informovat rodičovskou komponentu o dokončení
+        }
+      } else if (lettersLeft === userInput) {
+        onFinish(); // Pokud uživatel napsal správně poslední písmenko
+      }
+    };
+
+    document.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      document.removeEventListener('keyup', handleKeyUp);
+    };
+  }, [lettersLeft, onFinish]);
+
+  return (
+    <div className="wordbox">{lettersLeft}</div>
+  );
+};
+
+export default Wordbox;
+
+
+/* První kod
 import React, { useState } from 'react';
 import './style.css';
 
@@ -9,4 +47,71 @@ const Wordbox = ({ word }) => {
   );
 };
 
-export default Wordbox;
+export default Wordbox;*/
+
+/*Kod s mazáním písmen
+import React, { useState, useEffect } from 'react';
+import './style.css';
+
+const Wordbox = ({ word }) => {
+  const [lettersLeft, setLettersLeft] = useState(word);  
+
+  useEffect(() => {
+    const handleKeyUp = (event) => {
+      const userInput = event.key;
+      if (lettersLeft.includes(userInput)) {
+        const updatedLetters = lettersLeft.replace(userInput, '');
+        setLettersLeft(updatedLetters);
+      }
+    };
+
+    document.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      document.removeEventListener('keyup', handleKeyUp);
+    };
+  }, [lettersLeft]);
+
+  return (
+    <div className="wordbox">{lettersLeft}</div>
+  );
+};
+
+export default Wordbox; */
+
+
+
+
+/* kod s přidáním props onFinish
+import React, { useState, useEffect } from 'react';
+import './style.css';
+
+const Wordbox = ({ word, onFinish }) => {
+  const [lettersLeft, setLettersLeft] = useState(word);  
+
+  useEffect(() => {
+    const handleKeyUp = (event) => {
+      const userInput = event.key;
+      if (lettersLeft.includes(userInput)) {
+        const updatedLetters = lettersLeft.replace(userInput, '');
+        setLettersLeft(updatedLetters);
+
+        if (updatedLetters === '') {
+          onFinish(); // Informovat rodičovskou komponentu o dokončení
+        }
+      }
+    };
+
+    document.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      document.removeEventListener('keyup', handleKeyUp);
+    };
+  }, [lettersLeft, onFinish]);
+
+  return (
+    <div className="wordbox">{lettersLeft}</div>
+  );
+};
+
+export default Wordbox;*/
